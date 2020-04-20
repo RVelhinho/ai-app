8ba4bf535ce434a1cc14bd5e87c2dadf7ce43536
RVelhinho <ricardo_velhinh96@hotmail.com>
2020-04-21 00:44:30 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Route, Switch} from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route path='/' component={}/>
+        </Switch>
+      </React.Fragment>
+    )
+  }
+}
+
