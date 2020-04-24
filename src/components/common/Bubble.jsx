da5e0ae7c3dd5542b350099b158734e2b9adf99c
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2020-04-24 16:37:17 +0100

    ups

--- src/components/common/Bubble.jsx
+++ src/components/common/Bubble.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import "../style/bubble.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
 
-export default function Bubble({ desc, icon }) {
+export default function Bubble({desc, icon}) {
     return (
         <React.Fragment>
-            <Link
-                to="/preferences"
-                className="col bubble d-flex align-items-center"
-                style={{
-                    cursor: "pointer",
-                    color: "#1a1a1a",
-                    textDecoration: "none",
-                }}
-            >
+            <Link to="/preferences" className="col bubble d-flex align-items-center mb-3"
+                style={
+                    {
+                        cursor: "pointer",
+                        color: "#1a1a1a",
+                        textDecoration: "none"
+                    }
+            }>
                 <div className="row w-100 pl-4 py-2">
                     <div className="col-12 col-sm-12 col-md-5 col-lg-5 d-flex align-items-center justify-content-center justify-content-md-start justify-content-lg-start">
-                        <h1 className="text1">{desc}</h1>
+                        <h1 className="text1">
+                            {desc}</h1>
                     </div>
                     <div className="col d-flex align-items-center justify-content-center justify-content-md-end justify-content-lg-end">
-                        <FontAwesomeIcon
-                            icon={icon}
+                        <FontAwesomeIcon icon={icon}
                             size="5x"
-                            className="icon"
-                        />
+                            className="icon"/>
                     </div>
                 </div>
             </Link>
